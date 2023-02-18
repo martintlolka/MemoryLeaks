d7bfde8b975bad5e34e0ff57151845ae4b9ea846
Martin Tlolka <martintlolka@gmail.com>
2023-02-18 15:31:14 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MemoryLeaks;
+
+Console.WriteLine($"Memory usage {GC.GetTotalMemory(true) / 1024}kb");
+
+var stack = new Stack(10000);
+
+Console.WriteLine($"Memory usage {GC.GetTotalMemory(true) / 1024}kb");
+
+for (int i = 0;i < 10000; i++)
+{
+    stack.Push($"Some string {i}");
+}
+
+for (int i = 0; i < 10000; i++)
+{
+    stack.Pop();
+}
+
+Console.WriteLine($"Memory usage {GC.GetTotalMemory(true) / 1024}kb");
---
+++ Stack.cs
@@ -0,0 +1,25 @@
+﻿namespace MemoryLeaks
+{
+    public class Stack
+    {
+        private object[] elements;
+        private int size = 0;
+
+        public Stack(int initialCapacity)
+        { 
+            this.elements = new object[initialCapacity];
+        }
+
+        public void Push(object o)
+        {
+            this.elements[size++] = o;
+        }
+
+        public object Pop()
+        {
+            var top = this.elements[--size];
+            this.elements[size] = null;
+            return top;
+        }
+    }
+}
